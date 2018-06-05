56da78a136f0b98abd3b87590999371706e9dc25
Majo Lind <mariajoselind97@gmail.com>
2018-06-05 11:11:59 -0300

    Ahora si creo que deberia estar

--- src/app/routes/news.js
+++ src/app/routes/news.js
@@ -4,13 +4,8 @@ module.exports = app => {
     const connection = dbConnection();
 
     app.get('/' , (req , res ) => {
-<<<<<<< HEAD
         connection.query('SELECT *   FROM new', (err, result) =>{ 
         res.render ('news/news' ,{
-=======
-        connection.query('SELECT *   FROM news', (err, result) => { 
-        res.render ('news/news', {
->>>>>>> 2c09ca6b3ca057684a0cfb8fcb828f564d47e23a
             news: result 
         });
     });
