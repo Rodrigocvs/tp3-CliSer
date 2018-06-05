9bb0b61b7804bb9f4a5bb1ba0fbb3d93e24adca4
Majo Lind <mariajoselind97@gmail.com>
2018-06-05 11:01:37 -0300

    Elimine conflicto

--- src/sql/database.sql
+++ src/sql/database.sql
@@ -5,19 +5,9 @@ USE new_database;
 CREATE table news (
     id_new INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     title VARCHAR (100),
-<<<<<<< HEAD
     NEW VARCHAR(1000)
 ); 
 
 DESCRIBE new;
 INSERT into new (title, new) values ('my title','contente of news');
 select * from new;
-=======
-    news TEXT,
-    date_created TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
-
-DESCRIBE news;
-INSERT into news (title, news) values ('my title','contente of news');
-select * from news;
->>>>>>> 2c09ca6b3ca057684a0cfb8fcb828f564d47e23a
